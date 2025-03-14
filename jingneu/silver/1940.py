d7b2b87a5780ac958e4f3ff904a3091813afc5fb
jieun <suspen064@gmail.com>
2025-03-14 17:51:57 +0900

    [Jieun] 10일차 silver 1940.py

---
+++ jingneu/silver/1940.py
@@ -0,0 +1,27 @@
+import sys
+input = lambda: sys.stdin.readline().strip()
+
+#재료의 개수
+N = int(input())
+#갑옷 만드는데 필요한 수
+M = int(input())
+arr = list(map(int, input().split(" ")))
+arr.sort()
+
+
+#두 개 합이 N이 될 수 있는 개수
+
+p1 = 0
+p2 = N-1
+
+ans = 0
+while p1<p2:
+    if arr[p1]+arr[p2] == M:
+        ans+=1
+        p1+=1
+    if arr[p1]+arr[p2] < M:
+        p1+=1
+    if arr[p1]+arr[p2] > M:
+        p2-=1
+        
+print(ans)
